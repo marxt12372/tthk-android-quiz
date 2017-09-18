29d54e94ff741547b1293cf11596a87e948b04c8
Mart Ammon <mart@liovic.eu>
2017-09-18 17:16:28 +0300

    Added connectors, clicklisteners, quiz changeing.

--- app/src/main/java/eu/marxt12372/quiz/Quiz.java
+++ app/src/main/java/eu/marxt12372/quiz/Quiz.java
@@ -2,12 +2,158 @@ package eu.marxt12372.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Quiz extends AppCompatActivity {
+import java.util.List;
+
+public class Quiz extends AppCompatActivity
+{
+    private static int maxScore;
+    private int question;
+    private int currentscore;
+    private Questions questions;
+
+    private TextView quiz;
+    private TextView score;
+    private Button[] btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        questions = new Questions();
+
+        quiz = (TextView) findViewById(R.id.quiz);
+        score = (TextView) findViewById(R.id.score);
+        btn = new Button[4];
+        btn[0] = (Button) findViewById(R.id.button);
+        btn[1] = (Button) findViewById(R.id.button2);
+        btn[2] = (Button) findViewById(R.id.button3);
+        btn[3] = (Button) findViewById(R.id.button4);
+
+        question = 0;
+        currentscore = 0;
+        maxScore = 0; //TODO: Salvestada file ja laadida filest???
+
+        changeQuestion(question);
+
+        btn[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btn[0].getText().toString().equals(questions.getCorrentAnswer(question))) //Õige vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerright), Toast.LENGTH_SHORT).show();
+                    currentscore++;
+                }
+                else //Vale vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerwrong), Toast.LENGTH_SHORT).show();
+                }
+
+                if(question < 4)
+                {
+                    question++;
+                    changeQuestion(question);
+                }
+                else if(question == 4)
+                {
+                    question = 0;
+                    //Show the score activity thingy.
+                }
+            }
+        });
+
+        btn[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btn[1].getText().toString().equals(questions.getCorrentAnswer(question))) //Õige vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerright), Toast.LENGTH_SHORT).show();
+                    currentscore++;
+                }
+                else //Vale vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerwrong), Toast.LENGTH_SHORT).show();
+                }
+
+                if(question < 4)
+                {
+                    question++;
+                    changeQuestion(question);
+                }
+                else if(question == 4)
+                {
+                    question = 0;
+                    //Show the score activity thingy.
+                }
+            }
+        });
+
+        btn[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btn[2].getText().toString().equals(questions.getCorrentAnswer(question))) //Õige vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerright), Toast.LENGTH_SHORT).show();
+                    currentscore++;
+                }
+                else //Vale vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerwrong), Toast.LENGTH_SHORT).show();
+                }
+
+                if(question < 4)
+                {
+                    question++;
+                    changeQuestion(question);
+                }
+                else if(question == 4)
+                {
+                    question = 0;
+                    //Show the score activity thingy.
+                }
+            }
+        });
+
+        btn[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btn[3].getText().toString().equals(questions.getCorrentAnswer(question))) //Õige vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerright), Toast.LENGTH_SHORT).show();
+                    currentscore++;
+                }
+                else //Vale vastus
+                {
+                    Toast.makeText(getBaseContext(), getString(R.string.answerwrong), Toast.LENGTH_SHORT).show();
+                }
+
+                if(question < 4)
+                {
+                    question++;
+                    changeQuestion(question);
+                }
+                else if(question == 4)
+                {
+                    question = 0;
+                    //Show the score activity thingy.
+                }
+            }
+        });
+    }
+
+    public void changeQuestion(int newQuestion)
+    {
+        quiz.setText(questions.getQuestion(newQuestion));
+        score.setText(currentscore + "/" + maxScore);
+        List<String> answers = questions.getAnswers(newQuestion);
+        for(int i = 0; i <= 3; i++)
+        {
+            btn[i].setText(answers.get(i));
+        }
     }
 }
