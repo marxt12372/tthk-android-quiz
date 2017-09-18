bf0d8b16e1ffaac4e11bcca156e62a2213649324
Mart Ammon <mart@liovic.eu>
2017-09-18 17:11:44 +0300

    Populated Questions.java

---
+++ app/src/main/java/eu/marxt12372/quiz/Questions.java
@@ -0,0 +1,48 @@
+package eu.marxt12372.quiz;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Questions
+{
+    private String MyQuestions [] = {
+            "Kys1",
+            "Kys2",
+            "Kys3",
+            "Kys4",
+            "Kys5"
+    };
+
+    private String MyChoices [] [] = {
+            {"Vastus1", "Vastus2", "Vastus3", "Vastus4"},
+            {"Vastus1", "Vastus2", "Vastus3", "Vastus4"},
+            {"Vastus1", "Vastus2", "Vastus3", "Vastus4"},
+            {"Vastus1", "Vastus2", "Vastus3", "Vastus4"},
+            {"Vastus1", "Vastus2", "Vastus3", "Vastus4"}
+    };
+
+    private String MyCorrectAnswers [] = {
+            "Vastus1", "Vastus1", "Vastus1", "Vastus1", "Vastus1"
+    };
+
+    public String getQuestion(int question)
+    {
+        return MyQuestions[question];
+    }
+
+    public List<String> getAnswers(int question)
+    {
+        List<String> answers = new ArrayList<String>();
+        for(int i = 0; i <= 3; i++)
+        {
+            answers.add(MyChoices[question][i]);
+        }
+        return answers;
+    }
+
+    public String getCorrentAnswer(int question)
+    {
+        return MyCorrectAnswers[question];
+    }
+}
