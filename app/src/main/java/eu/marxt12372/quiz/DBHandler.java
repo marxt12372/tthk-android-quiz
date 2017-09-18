18040610cee7af3ede611acc88be09b1d099b24f
Mart Ammon <mart@liovic.eu>
2017-09-18 18:18:58 +0300

    Updates to DBHandler

--- app/src/main/java/eu/marxt12372/quiz/DBHandler.java
+++ app/src/main/java/eu/marxt12372/quiz/DBHandler.java
@@ -1,6 +1,7 @@
 package eu.marxt12372.quiz;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -20,5 +21,22 @@ public class DBHandler extends SQLiteOpenHelper
     {
         db.execSQL("DROP TABLE IF EXISTS score");
         onCreate(db);
+    }
+
+    public int getMaxScore()
+    {
+        int maxScore = 0;
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM score WHERE id = 1;", null);
+        maxScore = c.getInt(c.getColumnIndex("score"));
+        db.close();
+        return maxScore;
+    }
+
+    public void setMaxScore(int newMaxScore)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("UPDATE score SET score = " + newMaxScore + " WHERE id = 1;");
+        db.close();
     }
 }
